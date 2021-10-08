925f81145cb9ef1d10b1ecf7516715a0d4c3fcfe
Abhishek Raj <abhishekraj272@gmail.com>
2021-10-08 16:02:25 +0530

    added babel and build script

--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,22 @@
 export const getImportDeclaration = (data, t) => {
-  const { name, lib, defaultImport } = data;
-  const identifier = t.identifier(name);
-  const importSpecifier = defaultImport
-    ? t.importDefaultSpecifier(identifier)
-    : t.importSpecifier(identifier, identifier);
-  return t.importDeclaration([importSpecifier], t.stringLiteral(lib));
+	const { name, lib, defaultImport } = data;
+	const identifier = t.identifier(name);
+	const importSpecifier = defaultImport
+		? t.importDefaultSpecifier(identifier)
+		: t.importSpecifier(identifier, identifier);
+	return t.importDeclaration([importSpecifier], t.stringLiteral(lib));
 };
 
 export const getInitData = (context) => {
-  if (
-    !Array.isArray(context.opts.imports) ||
-    !context.opts.imports.length > 0 ||
-    !context.opts.target
-  ) {
-    console.error(
-      "Error: Please provide Function Name, Lib and target correctly in babel config."
-    );
-    process.exit(1);
-  }
-  return { params: context.opts, func: context.opts.imports[0] };
+	if (
+		!Array.isArray(context.opts.imports) ||
+		!context.opts.imports.length > 0 ||
+		!context.opts.target
+	) {
+		console.error(
+			'Error: Please provide Function Name, Lib and target correctly in babel config.'
+		);
+		process.exit(1);
+	}
+	return { params: context.opts, func: context.opts.imports[0] };
 };
